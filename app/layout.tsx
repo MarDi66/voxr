1747e6649627673bf99d9df6cfa0e852f78f0c5e
MarDi66 <yohann.mtfpro@gmail.com>
2026-03-17 12:58:51 +0100

    feat: add Vercel Analytics integration to layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
           {children}
         </TooltipProvider>
         <Toaster richColors />
+        <Analytics />
       </body>
     </html>
   );
